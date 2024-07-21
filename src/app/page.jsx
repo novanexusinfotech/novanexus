64dd16529835c481afdca6427080d018f5cc45d3
Rajeshkumar_145 <rajeshkumarlogu145@gmail.com>
2024-07-21 17:20:21 +0530

    feat: test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import logo from "@/images/logo.jpg";
+import logo from '@/images/logo.jpg';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <Image src={logo} alt="Logo" width={200} height={200}/>
-     <h1 className='font-bold text-2xl'>NovaNexus Infotech</h1>
+      <Image src={logo} alt="Logo" width={200} height={200} />
+      <h1 className="font-bold text-2xl">NovaNexus Infotech</h1>
     </main>
   );
 }
