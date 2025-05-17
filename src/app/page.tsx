1656bc1105f55267625e3c53bc337303a4a133cb
Ratni957 <ratnamanir957@gmail.com>
2025-05-17 12:36:58 +0530

    feat: add typeScript support and new Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
-import logo from '@/images/logo.jpg';
+import logo from '../images/logo.jpg';
+import { FC } from 'react';
 
-export default function Home() {
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Image src={logo} alt="Logo" width={200} height={200} />
       <h1 className="font-bold text-2xl">NovaNexus Infotech</h1>
     </main>
   );
-}
+};
+
+export default Home;
